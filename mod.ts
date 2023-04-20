fc801e701cbce5f88d67456ed0b29b4a8fb64a18
X <i@jex.me>
2023-04-20 18:48:22 +0800

    0.0.3

--- mod.ts
+++ mod.ts
@@ -2,11 +2,12 @@ import { join } from "https://deno.land/std@0.180.0/path/mod.ts";
 import {
   transform,
   type TransformOptions,
-} from "https://deno.land/x/esbuild@v0.17.16/mod.js";
+} from "https://deno.land/x/esbuild@v0.17.17/mod.js";
 import { getContentType } from "./media_type.ts";
 import { getBrowserInfo } from "./util.ts";
 
 const moduleRegexp = /\.(js|mjs|jsx|ts|mts|tsx|css)$/;
+const localHostnames = new Set(["localhost", "127.0.0.1", "::1"]);
 
 type Options = {
   root?: string;
@@ -18,9 +19,9 @@ type Options = {
 export default async function assets(
   req: Request,
   options?: Options,
-  fallback?: (req: Request) => Promise<Response> | Response,
+  fallback?: () => Promise<Response> | Response,
 ): Promise<Response> {
-  const { pathname } = new URL(req.url);
+  const { hostname, pathname } = new URL(req.url);
   if (
     !pathname.startsWith("/.") &&
     !options?.ignore?.some((p) => pathname.startsWith(p))
@@ -30,13 +31,29 @@ export default async function assets(
       try {
         const filePath = join(options?.root ?? Deno.cwd(), pathname);
         const stat = await Deno.lstat(filePath);
-        const headers = new Headers({ "Content-Type": contentType });
         if (stat.isFile) {
-          let etag: string | null = null;
           const { mtime, size } = stat;
+          const headers = new Headers({ "Content-Type": contentType });
+          const isModule = options?.transform && moduleRegexp.test(pathname) &&
+            (
+              !(options.transform instanceof RegExp) ||
+              options.transform.test(pathname)
+            );
+          const transformOptions = options?.transformOptions ?? {};
+          if (isModule && !transformOptions.target) {
+            const browser = getBrowserInfo(req.headers.get("User-Agent"));
+            transformOptions.target = browser
+              ? browser.name + browser.version
+              : "es2015";
+          }
+          let etag: string | null = null;
           if (mtime) {
             etag = `W/${mtime.getTime().toString(16)}-${size.toString(16)}`;
-            headers.set("Last-Modified", new Date(mtime).toUTCString());
+            if (isModule) {
+              etag += `-${transformOptions.target}`;
+            } else {
+              headers.set("Last-Modified", new Date(mtime).toUTCString());
+            }
           } else {
             const deployId = Deno.env.get("DENO_DEPLOYMENT_ID");
             if (deployId) {
@@ -52,31 +69,31 @@ export default async function assets(
             headers.set("ETag", etag);
           }
           const file = await Deno.open(filePath, { read: true });
-          if (
-            options?.transform && moduleRegexp.test(pathname) &&
-            (!(options.transform instanceof RegExp) ||
-              options.transform.test(pathname))
-          ) {
-            const input = await new Response(file.readable).text();
-            const transformOptions = options.transformOptions ?? {};
-            if (!transformOptions.target) {
-              const browser = getBrowserInfo(req.headers.get("User-Agent"));
-              transformOptions.target = browser
-                ? browser.name + browser.version
-                : "es2015";
-            }
-            if (!transformOptions.format) {
-              transformOptions.format = "esm";
-            }
+          if (isModule) {
             let ext = pathname.split(".").pop();
             if (ext === "mts") {
               ext = "ts";
             } else if (ext === "mjs") {
               ext = "js";
             }
+            if (transformOptions.format === undefined) {
+              transformOptions.format = "esm";
+            }
+            if (
+              transformOptions.sourcemap === undefined &&
+              localHostnames.has(hostname)
+            ) {
+              transformOptions.sourcemap = "inline";
+            }
+            if (
+              transformOptions.minify === undefined &&
+              !localHostnames.has(hostname)
+            ) {
+              transformOptions.minify = true;
+            }
             try {
               const ret = await transform(
-                input,
+                await new Response(file.readable).text(),
                 Object.assign(
                   transformOptions,
                   {
@@ -93,7 +110,7 @@ export default async function assets(
               }
               return new Response(ret.code, { headers });
             } catch (err) {
-              console.log(err);
+              console.error(err);
               return new Response(err.message, { status: 500 });
             }
           }
@@ -101,18 +118,12 @@ export default async function assets(
         }
       } catch (err) {
         if (!(err instanceof Deno.errors.NotFound)) {
-          return new Response(err.message, {
-            status: 500,
-            headers: [["Content-Type", "text/html"]],
-          });
+          return new Response(err.message, { status: 500 });
         }
       }
     }
   }
-  if (fallback) {
-    return fallback(req);
-  }
-  return new Response("Not Found", { status: 404 });
+  return fallback?.() ?? new Response("Not Found", { status: 404 });
 }
 
 export { registerType } from "./media_type.ts";
--- version.ts
+++ version.ts
@@ -1,5 +1,5 @@
 /** `version` managed by https://deno.land/x/land/publish. */
-export const VERSION = "0.0.2";
+export const VERSION = "0.0.3";
 
 /** `postpublish` will be invoked after published. */
 export function postpublish(version: string) {
